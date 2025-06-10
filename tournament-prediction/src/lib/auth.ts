da65a4ee44efb51dd172c9bf3703fa90f1b669e3
Toni Vanjak <toni.vanjak@gmail.com>
2025-06-10 12:10:17 +0200

    Update auth.ts

--- tournament-prediction/src/lib/auth.ts
+++ tournament-prediction/src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import prisma from "@/lib/prisma-client";
+import { prisma } from "@/lib/prisma-client";
 
 
 export const authOptions: NextAuthOptions = {
